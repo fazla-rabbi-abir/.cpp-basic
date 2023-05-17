0f225be4aee2d74bd7efc5a37f44eb38cea0da88
Md Fazla Rabbi Abir <131839058+avroabir96@users.noreply.github.com>
2023-05-17 20:53:07 +0600

    Add files via upload

---
+++ 3-user input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+    cout << "Enter two integer number : ";
+    cin >> num1 >> num2; //cin = console input(user input)
+
+    sum = num1 + num2;
+    cout << "Summation of numbers = " << sum;
+
+    return 0;
+}
