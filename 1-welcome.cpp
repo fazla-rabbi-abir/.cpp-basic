6b8a6af3487cde393c245ce663aa23a45fff997e
Md Fazla Rabbi Abir <131839058+avroabir96@users.noreply.github.com>
2023-05-17 20:27:14 +0600

    Add files via upload

---
+++ 1-welcome.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  //iostream = input output stream & its a header file
+using namespace std; //standard
+
+int main() //function
+{
+    cout << "Assalamualaikum" << endl;  //cout = console out(print)
+    cout << "This is Mr Kamal & Mrs Shahina's obedient son Md. Fazla Rabbi Abir," << endl;
+    cout << "from United States of NOAKHALI.";
+
+    return 0; //successful termination
+}
---
+++ 2-variable declaration & initialization.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    //variable declare & intial(dynamic)
+    int num1 = 10, num2 = 20; //int = data-type, num1 = variable name, 20 = variable
+
+    float num3 = 10.2;
+    double num4 = 10.1234567;
+    char ch = 'a';
+
+    //variable print
+    cout << "Integer number = " << num1 << endl;
+    cout << "Integer number = " << num2 << endl;
+    cout << "Float number = " << num3 << endl;
+    cout << "Double number = " << num4 << endl;
+    cout << "Character = " << ch;
+
+    return 0;
+
+    getch();
+}
