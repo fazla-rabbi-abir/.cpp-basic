d030cadc2e8cc7f923704fe84897243af4c47d3b
Md Fazla Rabbi Abir <131839058+avroabir96@users.noreply.github.com>
2023-05-23 00:30:45 +0600

    Add files via upload

---
+++ 7-statement-looping-do while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i=1;
+
+  do
+  {
+      cout << "Abir" << endl;
+      i++;
+  }
+  while(i<=100);
+  cout << "End of do while loop";
+
+  return 0;
+}
---
+++ 7-statement-looping-while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=1; //initialization
+    while(i<=100) //condition
+    {
+        cout << i <<endl;
+        i++; //increment
+    }
+    cout << "End of for loop";
+
+    return 0;
+}
