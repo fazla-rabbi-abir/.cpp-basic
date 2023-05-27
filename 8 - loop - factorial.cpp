bf344ef5e75045539e583ce8ae057c219a552360
Md Fazla Rabbi Abir <131839058+avroabir96@users.noreply.github.com>
2023-05-27 22:24:28 +0600

    Add files via upload

---
+++ 8 - loop - factorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, fact=1;
+    cout << "Enter any integer number : ";
+    cin >> num;
+
+    for(int i=1; i<=num; i++)
+    {
+        fact = fact * i;
+    }
+    cout << "Magnitude of this factorial number  = " << fact;
+
+    return 0;
+}
